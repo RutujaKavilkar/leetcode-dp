fe7deb10e80ee49f36ffdac7dc9bfee50a2c9bad
RutujaKavilkar <rutujabkavilkar19@gmail.com>
2025-10-08 23:33:51 +0530

    Add BuyAndSellStock_WithTransactionFees

---
+++ java/BuyAndSellStockWithTransactionFees.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BuyAndSellStockWithTransactionFees {
+   
+        private static int f(int ind,int buy,int[]prices,int n,int[][] dp,int fee)
+    {
+        if(ind==n) return 0;
+        if(dp[ind][buy]!=-1) return dp[ind][buy];
+        int profit;
+        if(buy==1)
+        {
+            profit=Math.max(-prices[ind]+f(ind+1,0,prices,n,dp,fee),0+f(ind+1,1,prices,n,dp,fee));
+        }else
+        {
+            profit=Math.max(prices[ind]-fee+f(ind+1,1,prices,n,dp,fee),0+f(ind+1,0,prices,n,dp,fee));
+        }
+        return dp[ind][buy]=profit;
+        }
+    public int maxProfit(int[] prices, int fee){
+        int n=prices.length;
+        int[][] dp=new int [n][2];
+        for(int[] rows:dp) Arrays.fill(rows,-1);
+        return f(0,1,prices,n,dp,fee);
+    }
+    
+}
+
